533e11f684aaeddf92b7909df2325d9f23dbde4d
goulett <goulett@OA-S141-09>
2026-05-05 17:42:49 -0400

    chaser code

---
+++ Assets/Scrips/chaser.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class chaser : MonoBehaviour
+{
+    public Camera gameCamera;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentMousePosition = Mouse.current.position.ReadValue();
+       
+        Vector3 worldMousePosition = gameCamera.ScreenToWorldPoint(currentMousePosition);
+        worldMousePosition.z = 0f;
+        transform.position = worldMousePosition;
+      
+
+    }
+}
